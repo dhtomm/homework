68fb2ec8aabb3a0dc55f257f488f00926ed4bbd1
dhtomm <130130224+dhtomm@users.noreply.github.com>
2023-04-07 15:34:25 +0900

    Add files via upload

---
+++ Test_0405.java
@@ -0,0 +1,101 @@
+package day07;
+
+import java.util.Scanner;
+
+public class Test_0405 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		//문제1
+		/*
+		int score = sc.nextInt();
+		
+		if(score >= 60) {
+			System.out.println("합격");
+		}else {
+			System.out.println("불합격");
+		}
+		
+		
+		//문제2
+		int python = 60;
+		int java = 70;
+		int android = 80;
+		int C = 50;
+		int cplus = 30;
+		
+		int sub = 5;
+		
+		int total = python + java + android + C + cplus;
+		int aver;
+		
+		System.out.println("숫자를 입력하세요 1.합계 2.평균");
+		int num = sc.nextInt();
+		switch(num) {
+		
+		 case 1:
+			 	System.out.println("총점은 = " + total + "입니다.");
+			 	break;
+		
+		 case 2:
+			 aver = total/sub;
+			 System.out.println(aver);
+			 break;
+		default : 
+				System.out.println("숫자를 확인해 주세요");
+				break;
+		}
+		
+		//문제 3
+		
+		System.out.println("가로 길이를 입력하세요");
+		int width = sc.nextInt();
+		System.out.println("세로 길이를 입력하세요");
+		int length = sc.nextInt();
+		System.out.println("높이를 입력하세요");
+		int height = sc.nextInt();
+		
+		int area = width*length*height;
+		System.out.println("넓이는 = " + area);
+		
+		//문제 4
+		System.out.println("반지름을 입력하세요");
+		int r = sc.nextInt();
+		System.out.println("높이를 입력하세요");
+		int h = sc.nextInt();
+		float pie = 3.14f;
+		
+		float area = pie*(r*r)*h; 
+		
+		System.out.println("원기둥의 넓이는 = " + area);
+		
+		//문제 5
+		System.out.println("숫자를 입력하세요");
+		int num = sc.nextInt();
+		int avalue = num >= 0 ? num : -num;
+		System.out.println("절대값은 = " + avalue);
+
+		//문제 6
+		System.out.println("숫자를 입력하세요");
+		int x = sc.nextInt();
+		if(x > 0 && x != 0) {
+			if(x % 2 == 0) {
+				System.out.println("짝수입니다.");	
+			}else {
+				System.out.println("홀수 입니다.");
+			}
+		}else {
+			System.out.println("숫자를 확인해 주세요");
+		}
+		 */
+		
+		
+	
+		
+		
+		
+		
+		
+	sc.close();	
+	}
+}
